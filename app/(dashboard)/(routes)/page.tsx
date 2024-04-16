c95df0990610f9b60e0b51484c6b18d610b9b8ec
ekkirinaldi <ekkirinaldi@gmail.com>
2024-04-16 07:04:23 +0700

    add clerk authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,15 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">
-        Hello World
-      </p>
-      <Button variant="link">
-        Click me
-      </Button>
+      <UserButton 
+        afterSignOutUrl="/" 
+      />
     </div>
   );
 }
